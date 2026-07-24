cec7137784cc9c41c827ef49265491641694807b
poze05698-collab <poze05698@gmail.com>
2026-07-24 03:54:23 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 TOKEN = "8771309444:AAE522egqaCOPGaAZUqDCB-6zY6uK-y1lhg"
 
-GRUPO_ID = -1001234567890      # ID do grupo
+GRUPO_ID = -1003355182545     # ID do grupo
 GRUPO_LINK = "https://t.me/pitbullslotsofc"
 
-ADMIN_ID = 123456789           # Seu ID do Telegram
+ADMIN_ID = 6172813641           # Seu ID do Telegram
 
 VALOR_POR_CONVIDADO = 1.00
 VALOR_MINIMO_SAQUE = 15.00
