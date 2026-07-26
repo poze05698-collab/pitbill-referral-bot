9a3c89c46ad5d0f929d9453773a822f7596a4b1a
poze05698-collab <poze05698@gmail.com>
2026-07-25 21:45:38 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,34 +2,20 @@ import telebot
 
 from config import TOKEN
 
-# Banco
 from database import conn, cursor
 
-# Módulos
 from usuario import registrar_usuario
 from saques import registrar_saques
 from admin import registrar_admin
-
-# ==========================================
-# BOT
-# ==========================================
 
 bot = telebot.TeleBot(
     TOKEN,
     parse_mode="HTML"
 )
 
-# ==========================================
-# REGISTRAR MÓDULOS
-# ==========================================
-
 registrar_usuario(bot)
 registrar_saques(bot)
 registrar_admin(bot)
-
-# ==========================================
-# COMANDOS GERAIS
-# ==========================================
 
 @bot.message_handler(commands=["ping"])
 def ping(message):
@@ -39,7 +25,6 @@ def ping(message):
         "🏓 Online!"
     )
 
-
 @bot.message_handler(commands=["id"])
 def meu_id(message):
 
@@ -47,7 +32,6 @@ def meu_id(message):
         message,
         f"Seu ID é:\n\n{message.from_user.id}"
     )
-
 
 @bot.message_handler(commands=["ajuda"])
 def ajuda(message):
@@ -60,42 +44,12 @@ Comandos disponíveis:
 /start
 /id
 /ping
-
-Utilize os botões para navegar.
-
-Caso tenha dúvidas,
-entre em contato com o suporte.
 """
 
     bot.send_message(
         message.chat.id,
         texto
     )
-
-
-# ==========================================
-# BLOQUEAR MENSAGENS DESCONHECIDAS
-# ==========================================
-
-@bot.message_handler(func=lambda m: True)
-def desconhecido(message):
-
-    bot.reply_to(
-
-        message,
-
-        """
-❌ Comando não reconhecido.
-
-Utilize os botões do menu.
-"""
-
-    )
-
-
-# ==========================================
-# INICIAR
-# ==========================================
 
 print("=" * 40)
 print("BOT ONLINE")
