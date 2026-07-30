eb9050f3ad8c715a30e7071bad73ce999ef6a36e
poze05698-collab <poze05698@gmail.com>
2026-07-30 01:01:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,32 @@
 =========================================
 """
 
-from bot import bot
 import database
+
+from bot import bot
+from usuarios import cadastrar_usuario
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
+
+    # Cadastra o utilizador
+    cadastrar_usuario(message.from_user)
+
     nome = message.from_user.first_name
 
     bot.reply_to(
         message,
-        f"👋 Olá, <b>{nome}</b>!\n\n"
-        "🚀 Bem-vindo ao Pitbull Referral Bot V2.\n\n"
-        "O bot está funcionando com sucesso!"
+        f"""
+👋 Olá, <b>{nome}</b>!
+
+🎉 Seja bem-vindo ao <b>Pitbull Referral Bot V2</b>.
+
+✅ O seu cadastro foi realizado com sucesso!
+
+Em breve terá acesso ao sistema de afiliados, carteira e saques.
+""",
+        parse_mode="HTML"
     )
 
 
