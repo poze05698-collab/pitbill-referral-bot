b380fd5bd4c9286d5ea1b1024773837f25a458a6
poze05698-collab <poze05698@gmail.com>
2026-08-01 16:42:48 -0300

    Update admin_usuarios.py

--- admin_usuarios.py
+++ admin_usuarios.py
@@ -56,3 +56,78 @@ def menu_admin_usuarios():
     )
 
     return teclado
+# ==================================================
+# BUSCAR USUÁRIO
+# ==================================================
+
+from database import cursor
+
+def buscar_usuario_admin(usuario_id):
+
+    cursor.execute("""
+
+        SELECT *
+
+        FROM usuarios
+
+        WHERE id=?
+
+    """, (
+
+        usuario_id,
+
+    ))
+
+    return cursor.fetchone()# ==================================================
+# TEXTO DO USUÁRIO
+# ==================================================
+
+def texto_usuario_admin(usuario):
+
+    if usuario is None:
+
+        return "❌ Usuário não encontrado."
+
+    return f"""
+👤 <b>DADOS DO USUÁRIO</b>
+
+━━━━━━━━━━━━━━━━━━
+
+🆔 ID:
+<code>{usuario['id']}</code>
+
+👤 Nome:
+{usuario['nome']}
+
+🔖 Código:
+<code>{usuario['codigo']}</code>
+
+💰 Saldo:
+R$ {usuario['saldo']:.2f}
+
+👥 Indicados:
+{usuario['indicados']}
+
+⭐ Nível:
+{usuario['nivel']}
+
+🏆 XP:
+{usuario['xp']}
+
+👑 VIP:
+{usuario['vip']}
+
+💎 Premium:
+{'Sim' if usuario['premium'] else 'Não'}
+
+📢 Grupo:
+{'✅ Sim' if usuario['grupo_verificado'] else '❌ Não'}
+
+📣 Canal:
+{'✅ Sim' if usuario['canal_verificado'] else '❌ Não'}
+
+🚫 Banido:
+{'✅ Sim' if usuario['banido'] else '❌ Não'}
+
+━━━━━━━━━━━━━━━━━━
+"""
