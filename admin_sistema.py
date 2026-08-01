e5506bac3a39a3f7b9366240efd426f07cfe7607
poze05698-collab <poze05698@gmail.com>
2026-07-31 23:33:17 -0300

    Create admin_sistema.py

---
+++ admin_sistema.py
@@ -0,0 +1,76 @@
+"""
+==================================================
+PITBULL REWARDS PLATFORM V3
+Sistema Administrativo
+==================================================
+"""
+
+from config import OWNER_ID
+from database import conn, cursor, agora
+
+
+# ==================================================
+# CRIAR OWNER
+# ==================================================
+
+def criar_owner():
+
+    cursor.execute("""
+
+        SELECT *
+
+        FROM administradores
+
+        WHERE cargo='OWNER'
+
+    """)
+
+    owner = cursor.fetchone()
+
+    if owner:
+
+        return
+
+    cursor.execute("""
+
+        INSERT INTO administradores(
+
+            usuario_id,
+
+            cargo,
+
+            status,
+
+            criado_por,
+
+            created_at,
+
+            updated_at
+
+        )
+
+        VALUES(
+
+            ?,?,?,?,?,?
+
+        )
+
+    """, (
+
+        OWNER_ID,
+
+        "OWNER",
+
+        "ATIVO",
+
+        OWNER_ID,
+
+        agora(),
+
+        agora()
+
+    ))
+
+    conn.commit()
+
+    print("✅ OWNER criado automaticamente.")
