4be06f38408a2d4a6cb27f1a3fa9b9b1c040d5de
poze05698-collab <poze05698@gmail.com>
2026-08-04 01:37:13 -0300

    Update logs.py

--- admin/logs.py
+++ admin/logs.py
@@ -1,1 +1,84 @@
-# Em desenvolvimento
+"""
+==================================================
+PITBULL REWARDS PLATFORM V3
+ADMIN - LOGS
+==================================================
+"""
+
+from database import (
+    conn,
+    cursor,
+    agora
+)
+
+# ==================================================
+# REGISTRAR LOG
+# ==================================================
+
+def registrar_log(
+
+    admin_id,
+    categoria,
+    descricao,
+    usuario_id=None
+
+):
+
+    data = agora()
+
+    cursor.execute("""
+
+    INSERT INTO logs(
+
+        admin_id,
+
+        usuario_id,
+
+        categoria,
+
+        descricao,
+
+        created_at
+
+    )
+
+    VALUES(?,?,?,?,?)
+
+    """,(
+
+        admin_id,
+
+        usuario_id,
+
+        categoria,
+
+        descricao,
+
+        data
+
+    ))
+
+    conn.commit()
+
+    return True
+
+
+# ==================================================
+# BUSCAR LOG
+# ==================================================
+
+def obter_log(log_id):
+
+    cursor.execute("""
+
+    SELECT *
+
+    FROM logs
+
+    WHERE id=?
+
+    """,(log_id,))
+
+    return cursor.fetchone()
+
+  
